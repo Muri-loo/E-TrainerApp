559f411d2a9d96bae841d66e999a5514ebc63aa1
Muri-loo <murilo936u@gmail.com>
2023-12-10 03:02:27 +0000

    viado de merda

--- App/Screens/Profile.js
+++ App/Screens/Profile.js
@@ -9,24 +9,32 @@ import {
 } from 'react-native';
 import Fundo from '../Navigation/fundo';
 import Navbar from '../Navigation/navbar';
-import { db } from "../../Config/firebase";
-import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
-import { getAuth, signOut } from "firebase/auth";
+import { db } from '../../Config/firebase';
+import {
+  doc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from 'firebase/firestore';
+import { getAuth, signOut } from 'firebase/auth';
 
 function Profile({ navigation }) {
-  
   const [athlete, setAthlete] = useState(null);
   const [userType, setUserType] = useState(null);
-  const [students, setStudents] = useState([]); 
+  const [students, setStudents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const logout = () => {
     const auth = getAuth();
-    signOut(auth).then(() => {
-      navigation.navigate("Home");
-    }).catch((error) => {
-      console.log("Error on signout", error);
-    });
+    signOut(auth)
+      .then(() => {
+        navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log('Error on signout', error);
+      });
   };
 
   const auth = getAuth();
@@ -34,31 +42,31 @@ function Profile({ navigation }) {
 
   const checkUserType = async () => {
     try {
-      // Attempt to fetch from the Atleta collectio
       console.log(userId);
-      const AthletQueryResult = await getDocs(query(collection(db, 'Atleta'), where('idAtleta', '==', userId)));
+      const AthletQueryResult = await getDocs(
+        query(collection(db, 'Atleta'), where('idAtleta', '==', userId))
+      );
       const atleta = AthletQueryResult.docs[0];
-      console.log(atleta.data());
 
-      if (AthletQueryResult.size>0) {
+      if (AthletQueryResult.size > 0) {
         setAthlete(atleta.data());
         setUserType('Atleta');
       } else {
         // If not found in Atleta, attempt to fetch from the Treinador collection
-    //    userRef = doc(db, 'Treinador', userId);
-      //  userSnapshot = await getDoc(userRef);
+        // userRef = doc(db, 'Treinador', userId);
+        // userSnapshot = await getDoc(userRef);
 
-        //if (userSnapshot.exists()) {
-          //setUserType('Treinador');
-          //const studentsQuery = query(collection(db, 'Atleta'), where('idTreinador', '==', userId));
-          //const studentsSnapshot = await getDocs(studentsQuery);
-          //setStudents(studentsSnapshot.docs.map(doc => doc.data()));
-        //} else {
-         // console.log("No user found with the given ID in either Atleta or Treinador collections.");
-      //  }
+        // if (userSnapshot.exists()) {
+        //   setUserType('Treinador');
+        //   const studentsQuery = query(collection(db, 'Atleta'), where('idTreinador', '==', userId));
+        //   const studentsSnapshot = await getDocs(studentsQuery);
+        //   setStudents(studentsSnapshot.docs.map(doc => doc.data()));
+        // } else {
+        //   console.log("No user found with the given ID in either Atleta or Treinador collections.");
+        // }
       }
     } catch (error) {
-      console.error("Error checking user type:", error);
+      console.error('Error checking user type:', error);
       setIsLoading(false);
     }
   };
@@ -66,7 +74,7 @@ function Profile({ navigation }) {
   useEffect(() => {
     const auth = getAuth();
     const userId = auth.currentUser.uid;
-    
+
     checkUserType();
   }, []);
 
@@ -77,143 +85,106 @@ function Profile({ navigation }) {
     return Math.abs(age_dt.getUTCFullYear() - 1970);
   };
 
-
-  if (userType === 'Atleta') {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Navbar navigation={navigation} />
-        <ScrollView style={styles.contentContainer}>
-          <Text style={styles.info}>{athlete.nome}</Text>
-          <Text style={styles.info}>Gender: {athlete.genero}</Text>
-          <Text style={styles.info}>Age: {athlete.dataNascimento}</Text>
-          <Text style={styles.info}>Atleta</Text>
-          <Text style={styles.info}>Height: {athlete.altura} cm</Text>
-          <Text style={styles.info}>Weight: {athlete.peso} kg</Text>
-          <Text style={styles.info}>Email: {athlete.email}</Text>
-          {/* Add your profile picture and other details here */}
-          {/* ... Other athlete-specific UI components ... */}
-          <TouchableOpacity style={styles.logoutButton} onPress={logout}>
-          <Text style={styles.info}>LOGOUT</Text>
-          </TouchableOpacity>
-        </ScrollView>
-        <Fundo navigation={navigation} />
-      </SafeAreaView>
-    );
-  }
-
-  if (userType === 'Treinador') {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Navbar navigation={navigation} />
-        <ScrollView style={styles.container}>
-          <Text style={styles.title}>Treinador</Text>
-          {/* Render the list of students here */}
-          <TouchableOpacity onPress={logout}>
-            <Text>LOGOUT</Text>
-          </TouchableOpacity>
-        </ScrollView>
-        <Fundo navigation={navigation} />
-      </SafeAreaView>
-    );
-  }
-
-  // Default return if userType is neither 'Atleta' nor 'Treinador'
-  return (
-    <SafeAreaView style={styles.container}>
-    <Navbar navigation={navigation} />
-    <ScrollView style={styles.container}>
-    <Text style={styles.loading}>No profile found</Text>
-      {/* You can add more athlete-specific UI components here */}
+  const renderAtletaProfile = () => (
+    <ScrollView style={styles.contentContainer}>
+      <Text style={styles.header}>{athlete.nome}</Text>
+      <Text style={styles.info}>Gender: {athlete.genero}</Text>
+      <Text style={styles.info}>Age: {athlete.dataNascimento}</Text>
+      <Text style={styles.subHeader}>Atleta</Text>
+      <Text style={styles.info}>Height: {athlete.altura} cm</Text>
+      <Text style={styles.info}>Weight: {athlete.peso} kg</Text>
+      <Text style={styles.info}>Email: {athlete.email}</Text>
+      {/* Add your profile picture and other details here */}
+      {/* ... Other athlete-specific UI components ... */}
+      <TouchableOpacity style={styles.logoutButton} onPress={logout}>
+        <Text style={styles.logoutText}>LOGOUT</Text>
+      </TouchableOpacity>
     </ScrollView>
-    <TouchableOpacity onPress={logout}>
-      <Text>LOGOUT</Text>
-    </TouchableOpacity>
-    <Fundo navigation={navigation} />
-  </SafeAreaView>
   );
 
-  
+  const renderTreinadorProfile = () => (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Treinador</Text>
+      {/* Render the list of students here */}
+      <TouchableOpacity onPress={logout}>
+        <Text style={styles.logoutText}>LOGOUT</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+
+  const renderDefaultProfile = () => (
+    <ScrollView style={styles.container}>
+      <Text style={styles.loading}>No profile found</Text>
+      {/* You can add more athlete-specific UI components here */}
+      <TouchableOpacity onPress={logout}>
+        <Text style={styles.logoutText}>LOGOUT</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navbar navigation={navigation} />
+      {userType === 'Atleta' && renderAtletaProfile()}
+      {userType === 'Treinador' && renderTreinadorProfile()}
+      {(!userType || (userType !== 'Atleta' && userType !== 'Treinador')) &&
+        renderDefaultProfile()}
+      <Fundo navigation={navigation} />
+    </SafeAreaView>
+  );
 }
 
-const styles=StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-      },
-      info:{
-        color:'#fff',
-      },
-      title: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 10,
-        alignSelf: 'center',
-      },
-      loading: {
-        marginTop: 300,
-        alignSelf: 'center',
-      },
-      profileSection: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        marginVertical: 20,
-      },
-      profilePic: {
-        width: 80,
-        height: 80,
-        borderRadius: 40,
-        backgroundColor: '#555', // Placeholder for image
-      },
-      detailsContainer: {
-        justifyContent: 'center',
-      },
-      detailText: {
-        color: '#fff',
-        fontSize: 16,
-      },
-      email: {
-        color: '#fff',
-        fontSize: 16,
-        marginVertical: 10,
-        alignSelf: 'center',
-      },
-      sectionTitle: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginVertical: 10,
-      },
-      description: {
-        color: '#fff',
-        fontSize: 16,
-        marginHorizontal: 20,
-        marginBottom: 20,
-      },
-      studentsContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        marginVertical: 10,
-      },
-      student: {
-        alignItems: 'center',
-        marginVertical: 10,
-      },
-      studentPic: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        backgroundColor: '#555', // Placeholder for image
-      },
-      studentName: {
-        color: '#fff',
-        fontSize: 14,
-        marginTop: 5,
-      },
-    });
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  contentContainer: {
+    padding: 20,
+  },
+  header: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subHeader: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  info: {
+    color: '#fff',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  loading: {
+    marginTop: 300,
+    alignSelf: 'center',
+    color: '#fff',
+    fontSize: 18,
+  },
+  logoutButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  logoutText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
 
 export default Profile;
