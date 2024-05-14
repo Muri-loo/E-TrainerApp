4465d09a1c794979d1b4008e6b63f5871815a675
Muri-loo <murilo936u@gmail.com>
2024-05-15 00:34:11 +0100

    b

--- App/Screens/LiveTraining.js
+++ App/Screens/LiveTraining.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Navbarlight from '../Navigation/navbarlight';
 import Fundo from '../Navigation/fundo';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity,Image } from 'react-native';
 import IconFA from 'react-native-vector-icons/FontAwesome5';
 import IconMC from 'react-native-vector-icons/MaterialCommunityIcons';
+import { db, auth } from '../../Config/firebase';
+import { addDoc, collection } from 'firebase/firestore';
+
 
 function LiveTraining({ navigation, route }) {
   const {lista,idPlanoTreino} = route.params;
@@ -47,7 +50,7 @@ function LiveTraining({ navigation, route }) {
     if (!isTrainingFinished) {
       exerciseNumber++;
       if (exerciseNumber >= exercises.length) {
-        handleFinishTraining();
+        setIsTrainingFinished(true); 
       } else {
         setCurrentExercise(exercises[exerciseNumber]);
       }
@@ -56,30 +59,38 @@ function LiveTraining({ navigation, route }) {
 
 
 
-  const handleFinishTraining = () => {
+  const handleFinishTraining = async () => {
     setIsTrainingFinished(true);
   
     // Calculate average speed and strength per punch
     console.log(punches);
     const totalPunchStrength = punches.reduce((acc, punch) => acc + punch.strength, 0);
     const averageStrengthPerPunchNewton = (totalPunchStrength / punches.length).toFixed(2);
-    
   
     // Find the strongest punch
     const strongestPunchStrength = punches.reduce((max, punch) => (punch.strength > max ? punch.strength : max), punches[0].strength);
+    const id = auth.currentUser.uid;
   
     // Populate FinishedTraining object
-    const FinishedTraining = {
-      AverageSpeedPerPunch: (seconds/punches.length).toFixed(2),
+    const finishedTraining = {
+      AverageSpeedPerPunch: (seconds / punches.length).toFixed(2),
       AverageStrengthPerPunchNewton: averageStrengthPerPunchNewton, // Assuming 1 kgf equals 9.81 newtons
       DurationOfTrainning: formatTime(seconds),
       NumberOfPunches: punches.length,
       PlanTrainId: idPlanoTreino, // Get this from your application's context or state management
       StrongestPunch: strongestPunchStrength,
+      idUtilizador: id,
     };
   
-    console.log('Finished Training:', FinishedTraining);
+    try {
+      await addDoc(collection(db, 'FinishedTrain'), finishedTraining);
+      console.log('Finished Training:', finishedTraining);
+    } catch (error) {
+      console.error('Error adding finished training: ', error);
+      alert('An error occurred while adding the finished training.');
+    }
   };
+  
   
   
 
@@ -92,10 +103,16 @@ function LiveTraining({ navigation, route }) {
   return (
     <SafeAreaView style={styles.container}>
       <Navbarlight navigation={navigation} />
+      
+      <Image source={{
+            uri: exercise.fotoExercicio ? exercise.fotoExercicio : 'https://drive.google.com/uc?export=view&id=1uNBArFrHi5f8c0WOlCHcJwPzWa8bKihV'
+      }}
+  style={styles.image}
+/>
       <View style={styles.content}>
         <View style={styles.topLeft}>
-          <Text style={{ fontSize: 18 }}>
-            <Text style={{ fontWeight: 'bold' }}>Exercício:</Text> {exercise.nomeExercicio}
+        <Text style={{ fontSize: 18 }}>
+            <Text style={{ fontWeight: 'bold', color: '#F01801' }}>Exercicio:</Text> {exercise.nomeExercicio}
           </Text>
         </View>
         <View style={styles.topLeft}>
@@ -103,6 +120,10 @@ function LiveTraining({ navigation, route }) {
             <Text style={{ fontWeight: 'bold', color: '#F01801' }}>Descrição:</Text> {exercise.descricao}
           </Text>
         </View>
+          <Text style={{ fontSize: 18,marginBottom:'5%' }}>
+            <Text style={{ fontWeight: 'bold', color: '#F01801' }}>Tempo recomendado :</Text> {formatTime(exercise.tempo)} <IconFA name={"clock"} size={18} color="black" />
+
+          </Text>
         <View style={styles.line}></View>
         <Text style={styles.timer}>{formatTime(seconds)}</Text>
         <Text style={{ fontSize: 15, alignSelf: 'center' }}>Este botão tem como{'\n'}objetivo simular um soco</Text>
@@ -120,11 +141,13 @@ function LiveTraining({ navigation, route }) {
             )}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleToggleFoward}>
-          <Text style={{ color: 'white' }}>
-            <IconFA name="forward" size={15} color="white" />
-          </Text>
-        </TouchableOpacity>
+        {!isTrainingFinished && (
+    <TouchableOpacity style={styles.button} onPress={handleToggleFoward}>
+      <Text style={{ color: 'white' }}>
+        <IconFA name="forward" size={15} color="white" />
+      </Text>
+    </TouchableOpacity>
+  )}
         <TouchableOpacity style={[styles.button, { marginLeft: '20%' }]} onPress={handleFinishTraining}>
           <Text style={{ color: 'white' }}>Terminar Treino</Text>
         </TouchableOpacity>
@@ -140,12 +163,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'space-between',
   },
+  image: {
+    width: '100%',
+    height: '20%',
+    borderBottomLeftRadius: 30,
+    marginTop: '-10%',
+    zIndex: -1,
+  },
   line: {
     borderWidth: 1,
     borderColor: '#CC2C02',
     backgroundColor: '#CC2C02',
     width: '100%',
     alignSelf: 'center',
+    marginBottom:'5%',
   },
   content: {
     flex: 1,
